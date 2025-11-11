d4fa21385206bba8ea113d7918cfa83179e71200
Ricardo Batista <iamricardobatista@gmail.com>
2025-11-11 22:25:44 +0000

    feat: endpoint with n0 dns

--- server-cli/src/error/mod.rs
+++ server-cli/src/error/mod.rs
@@ -1,4 +1,8 @@
 use derive_more::From;
+use iroh::endpoint;
 
 #[derive(Debug, From)]
-pub enum Error {}
+pub enum Error {
+    #[from]
+    Bind(endpoint::BindError),
+}
--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -1,8 +1,11 @@
+use iroh::Endpoint;
 use server_cli::{Result, instrument};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     instrument::init();
+
+    let endpoint = Endpoint::bind().await?;
 
     println!("Hello, Async World");
     Ok(())
