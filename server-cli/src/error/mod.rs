f4ca63aa0418ab059f7ad182c148593916484620
Ricardo Batista <iamricardobatista@gmail.com>
2025-11-16 20:59:54 +0000

    feat: subscribe loop

--- server-cli/src/error/mod.rs
+++ server-cli/src/error/mod.rs
@@ -1,8 +1,16 @@
 use derive_more::From;
 use iroh::endpoint;
+use iroh_gossip::api::ApiError;
+use tokio::task::JoinError;
 
 #[derive(Debug, From)]
 pub enum Error {
     #[from]
     Bind(endpoint::BindError),
+
+    #[from]
+    Join(JoinError),
+
+    #[from]
+    Api(ApiError),
 }
--- server-cli/src/main.rs
+++ server-cli/src/main.rs
@@ -1,12 +1,49 @@
-use iroh::Endpoint;
+use futures_lite::StreamExt;
+use iroh::{Endpoint, SecretKey, protocol::Router};
+use iroh_gossip::{
+    Gossip, TopicId,
+    api::{Event, GossipReceiver},
+};
 use server_cli::{Result, instrument};
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     instrument::init();
 
-    let endpoint = Endpoint::bind().await?;
+    let secret_key = SecretKey::generate(&mut rand::rng());
+    info!("server: secret key [{:?}]", secret_key.public());
 
-    println!("Hello, Async World");
+    let endpoint = Endpoint::builder().secret_key(secret_key).bind().await?;
+    info!("server: endpoint binded [{:?}]", endpoint.id());
+
+    let gossip = Gossip::builder().spawn(endpoint.clone());
+
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn();
+
+    let topic_id = TopicId::from_bytes(rand::random());
+    let peer_ids = vec![];
+
+    let (_sender, receiver) = gossip.subscribe(topic_id, peer_ids).await?.split();
+
+    tokio::spawn(subscribe_loop(receiver));
+
+    router.shutdown().await?;
+    info!("server: router shutdown");
+
+    endpoint.close().await;
+    info!("server: endpoint closed");
+    Ok(())
+}
+
+async fn subscribe_loop(mut receiver: GossipReceiver) -> Result<()> {
+    while let Some(gossip_event) = receiver.try_next().await? {
+        match gossip_event {
+            Event::Received(message) => info!("Event Received: {:?}", &message),
+            _ => info!("Gossip Event: {:?}", &gossip_event),
+        }
+    }
     Ok(())
 }
